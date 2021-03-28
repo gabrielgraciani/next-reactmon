a9384f5a596e9e2a6a1e9a55bf4a77090697a7d5
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-03-28 15:33:07 -0300

    added styled-components and baseUrl to the project

---
+++ client/src/styles/global.ts
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body, input, textarea, select, button {
+    font: 400 1rem "Roboto", sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyles;
