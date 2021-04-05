e8751c87ac3f3de94da8b071a915012d6dcc5657
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-04-05 20:37:04 -0300

    added put route user

--- server/src/modules/users/controllers/UsersController.ts
+++ server/src/modules/users/controllers/UsersController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import ListUsersService from '../services/ListUsersService';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserService from '../services/UpdateUserService';
 
 class UsersController {
   async index(request: Request, response: Response): Promise<Response> {
@@ -16,6 +17,17 @@ class UsersController {
 
     const createUser = new CreateUserService();
     const user = await createUser.execute({ name, email, password });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { name, email, password } = request.body;
+
+    const updateUser = new UpdateUserService();
+
+    const user = await updateUser.execute({ id, name, email, password });
 
     return response.json(user);
   }
--- server/src/modules/users/routes/users.routes.ts
+++ server/src/modules/users/routes/users.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import ensureAuthenticated from '../../../middlewares/ensureAuthenticated';
+
 import UsersController from '../controllers/UsersController';
 
 const usersRouter = Router();
@@ -7,5 +9,6 @@ const usersController = new UsersController();
 
 usersRouter.get('/', usersController.index);
 usersRouter.post('/', usersController.create);
+usersRouter.put('/:id', ensureAuthenticated, usersController.update);
 
 export default usersRouter;
