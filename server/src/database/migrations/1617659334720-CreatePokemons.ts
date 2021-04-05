72ed45a9b380257519b2d95ef44c18169d4d401f
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-04-05 18:54:39 -0300

    adding typeorm with postgres to server

---
+++ server/src/database/migrations/1617659334720-CreatePokemons.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreatePokemons1617659334720 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'pokemons',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'main_type',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('pokemons');
+  }
+}
