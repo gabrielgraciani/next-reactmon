b29df15e70f234f8156813629dfb7b1db7dce072
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-04-09 19:08:33 -0300

    added items image

--- server/src/database/migrations/1617980909933-SeedItems.ts
+++ server/src/database/migrations/1617980909933-SeedItems.ts
@@ -7,12 +7,12 @@ export default class SeedItems1617980909933 implements MigrationInterface {
     let data = '';
 
     SeedItems.forEach(seed => {
-      data = `${data} ('${seed.name}', '${seed.description}', '${seed.function}'),`;
+      data = `${data} ('${seed.name}', '${seed.description}', '${seed.function}', '${seed.image}'),`;
     });
 
     const dataSlice = data.slice(0, -1);
 
-    const query = `INSERT INTO items (name, description, function) VALUES ${dataSlice};`;
+    const query = `INSERT INTO items (name, description, function, image) VALUES ${dataSlice};`;
 
     await queryRunner.query(query);
   }
